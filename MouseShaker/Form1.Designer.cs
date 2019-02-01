23158047b74934640391bbe75640312b18f264c6
Jacob Gainey <jgainey@carolina.rr.com>
2019-02-01 14:17:51 -0500

    project start

--- MouseShaker/Form1.Designer.cs
+++ MouseShaker/Form1.Designer.cs
@@ -29,12 +29,109 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblCoordY = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblCoordX = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(179, 65);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(82, 22);
+            this.btnChange.TabIndex = 7;
+            this.btnChange.Text = "button1";
+            this.btnChange.UseVisualStyleBackColor = true;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(20, 75);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(101, 13);
+            this.lblMessage.TabIndex = 6;
+            this.lblMessage.Text = "Mouse Pointer = still";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblCoordY);
+            this.groupBox2.Location = new System.Drawing.Point(145, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(117, 47);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mouse Y Coordinate";
+            // 
+            // lblCoordY
+            // 
+            this.lblCoordY.AutoSize = true;
+            this.lblCoordY.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCoordY.Location = new System.Drawing.Point(18, 21);
+            this.lblCoordY.Name = "lblCoordY";
+            this.lblCoordY.Size = new System.Drawing.Size(14, 13);
+            this.lblCoordY.TabIndex = 0;
+            this.lblCoordY.Text = "0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblCoordX);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(117, 47);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mouse X Coordinate";
+            // 
+            // lblCoordX
+            // 
+            this.lblCoordX.AutoSize = true;
+            this.lblCoordX.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCoordX.Location = new System.Drawing.Point(18, 21);
+            this.lblCoordX.Name = "lblCoordX";
+            this.lblCoordX.Size = new System.Drawing.Size(14, 13);
+            this.lblCoordX.TabIndex = 0;
+            this.lblCoordX.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(274, 96);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mouse Shaker";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblCoordY;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblCoordX;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- MouseShaker/Form1.cs
+++ MouseShaker/Form1.cs
@@ -1,20 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace MouseShaker
 {
     public partial class Form1 : Form
     {
+
+        // We need to use unmanaged code
+        [DllImport("user32.dll")]
+        // GetCursorPos() makes everything possible
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        static int shaker = 0;
+        static int x = 0;
+        static int y = 0;
+
+        static string direction = null;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = 10;
+            timer2.Enabled = true;
+            timer2.Interval = 1000;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // New point that will be updated by the function with the current coordinates
+            Point currentCoordinates = new Point();
+
+            // Call the function and pass the Point
+            GetCursorPos(ref currentCoordinates);
+
+            // Now after calling the function, defPnt contains the coordinates which we can read
+            lblCoordX.Text = "X = " + currentCoordinates.X.ToString();
+            lblCoordY.Text = "Y = " + currentCoordinates.Y.ToString();
+
+            //if (currentCoordinates.X == x) { direction = "still"; }
+
+            if (currentCoordinates.X < x) { direction = "left"; }
+            if (currentCoordinates.X > x) { direction = "right"; }
+
+            x = currentCoordinates.X;
+
+            lblMessage.Text = "Mouse Pointer = " + direction;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (direction != direction) { shaker += 1; }
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            //Cursor cur = new Cursor(Properties.Resources.extralargearrow.Handel);
+            //this.Cursor = cur;
+        }
+
     }
 }
